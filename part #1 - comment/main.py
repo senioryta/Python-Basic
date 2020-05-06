095d5ff5e5f20eaa3cf6ceeddc0b74f8f0495126
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-06 14:42:44 -0700

    Update Part #1 - comment, file main.py

--- part #1 - comment/main.py
+++ part #1 - comment/main.py
@@ -1,4 +1,9 @@
-# this is the first comment 
-spam = 1 # this is the second comment
-		 # ... and now third!
-text = "# this is is not a comment because it's inside quotes"
+# ida  ne'e komentariu primeiru
+spam = 1 # ida ne'e mos komentariu segundu
+		# .. no agora terseiru
+text = "# ida ne'e laos komentariu tamba iha quote nia laran!"
+
+# iha python komentariu hakerek ho crus ou # 
+# no makina sei la ezekuta ou fo sai iha output!
+# komentariu, comment
+# this is a comment! use crus
