766cf07cad677796dde76b10463cfc654030d4f8
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-06 15:20:01 -0700

    Learn using python as calculator and number have type int & float, Part #2, file main.py

---
+++ Part #2 - Using python as calculator/main.py
@@ -0,0 +1,47 @@
+# the operators:
+"""
++ => addititon
+- => subtraction
+* => multiplication
+/ => division 
+
+and (()) parentheses can used  for grouping
+
+"""
+
+print(5 + 5)
+print(100 + 23)
+print(8 - 8)
+print(12 - 3 * 2)
+print(9 * 6)
+print((12 - 3) * 2)
+print(10 / 2) # division always return a  floating point number
+print(8 / 4)
+print(55 / 7)
+
+"""
+the intege number (e.g. 1,2,3,12,34,100) have type int
+
+the floating number (e.g 5.0,1.9,199.2) have type float
+"""
+
+# division always returns float, you can use the // (floor division) operator  and get type int
+
+print(10 // 2) 
+print(8 // 4)
+print(55 // 7)
+print(100 // 50)
+
+# to caltulate remainder, use %
+print(10 % 3) # the % operator returns the remainder of division
+
+# exponent, the ** operators
+print(10**2) # 10 squared or 10*10
+print(7**3) # 7 squared or 7*7*7
+print(2**8) # 2 the power of 8
+
+# The equal sign (=) is used to assign a value to a variable
+width = 300
+height = 200 * 2
+print(width + height)
+
