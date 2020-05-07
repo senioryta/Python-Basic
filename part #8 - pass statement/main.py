2352dcf45ba7af9beb2e8d56f012e4ba293393a0
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-06 22:00:35 -0700

    Learn pass statement in part #8, file main.py

---
+++ part #8 - pass statement/main.py
@@ -0,0 +1,27 @@
+"""
+# create empty loops
+while True:
+	pass # (ctrl + C) keyboard interrupt
+"""
+
+# create emptyclass
+class Myclass:
+	pass
+
+# empty function
+def empty(arg):
+	pass
+
+empty("hello word")
+
+for i in range(3,40,3):
+	if i == 24:
+		print("your number is " + str(i))
+		print("check 1!")
+		pass
+		print("Check 2!")
+
+	print(i)
+
+else:
+	print("outside loops!")
