ead145938f2baae90244ce17a8ac1723d2bd1a34
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-10 18:05:54 -0700

    try to try python reading & writing files in part #32, file main.py

--- part #32 - Reading & writing files/main.py
+++ part #32 - Reading & writing files/main.py
@@ -1,3 +1,4 @@
+"""
 file = open('jm.txt','w')
 
 file.write("hello, world!")
@@ -17,3 +18,24 @@ file3 = open('jm.txt','a')
 file3.write("\nnew line!")
 
 file3.close()
+"""
+
+htmlfile = open('index.html','w')
+
+htmlfile.write("<!DOCTYPE html>\n")
+htmlfile.write("<html>\n")
+htmlfile.write("<head>\n")
+htmlfile.write("\t<title>python r&w</title>\n")
+htmlfile.write("</head>\n")
+htmlfile.write("<body>\n")
+htmlfile.write("\t<h3>Hello, World</h3>\n")
+htmlfile.write("</body>\n")
+htmlfile.write("</html>")
+
+htmlfile.close()
+
+htmlread = open('index.html','r')
+
+print(htmlread.read())
+
+htmlread.close()
