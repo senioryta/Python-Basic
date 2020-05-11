ab7400ad24244b983bfe9f1c0d51ed08f6eadc39
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-10 17:47:29 -0700

    Learn the string format() method in part #31, file main.py

---
+++ part #31 - Format() method/main.py
@@ -0,0 +1,7 @@
+print("what is your {}? it's '{}'".format("name","jm"))
+
+print("{0} and {1}".format("watermelon","apple"))
+print("{1} and {0}".format("watermelon","apple"))
+
+print("{subject} is {object}".format(subject="he",object="programmer"))
+print("my {0} is {1} and i'm \"{hobby}\"".format("name","jm",hobby="prograrmmer"))
